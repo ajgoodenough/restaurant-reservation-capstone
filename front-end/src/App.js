25e4882c1361b01d238e9d9f3faaf2ea01ba6ab4
Andrew Goodenough <andrewgoodenough9@gmail.com>
2022-01-24 08:53:40 -0800

    frontend start

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
+import "./App.css";
 
-/**
- * Defines the root application component.
- * @returns {JSX.Element}
- */
 function App() {
   return (
-    <Switch>
-      <Route path="/">
-        <Layout />
-      </Route>
-    </Switch>
+    <div className="App">
+      <Switch>
+        <Route path="/">
+          <Layout />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
